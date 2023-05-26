206cef7d6d5d7cd82e640c5fd9fc2dbaefe908c1
Ramy Ashraf <Ramy3r@hotmail.com>
2023-05-26 18:25:27 +0300

    Implemented Repository Pattern and Remote Data source Class

--- app/src/main/java/com/example/itiworkshop_android/data/IRepository.kt
+++ app/src/main/java/com/example/itiworkshop_android/data/IRepository.kt
@@ -1,4 +1,17 @@
 package com.example.itiworkshop_android.data
 
+import com.example.itiworkshop_android.data.model.auth.AuthenticationResponse
+import com.example.itiworkshop_android.data.model.auth.LoginRequestBody
+import com.example.itiworkshop_android.data.model.auth.RegistrationRequestBody
+
 interface IRepository {
+    suspend fun register(body: RegistrationRequestBody) : AuthenticationResponse
+
+    suspend fun login(body: LoginRequestBody) : AuthenticationResponse
+
+    fun saveUserData(data: AuthenticationResponse.LoginResponseBody)
+
+    fun readUserData(): AuthenticationResponse
+
+    fun clearUserData()
 }
