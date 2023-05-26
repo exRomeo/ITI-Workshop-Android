4d2a036f4ea57e2247696eb99a65da634bb697d3
HP <nadaelshafey99@gmail.com>
2023-05-26 16:12:46 +0200

    navigation

--- app/src/main/java/com/example/itiworkshop_android/features/authentication/login/view/LoginFragment.kt
+++ app/src/main/java/com/example/itiworkshop_android/features/authentication/login/view/LoginFragment.kt
@@ -1,15 +1,18 @@
 package com.example.itiworkshop_android.features.authentication.login.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.itiworkshop_android.R
 import com.example.itiworkshop_android.data.model.User
 import com.example.itiworkshop_android.databinding.FragmentLoginBinding
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,7 +22,7 @@ private const val ARG_PARAM2 = "param2"
 
 class LoginFragment : Fragment() {
 
-    lateinit var binding : FragmentLoginBinding
+    lateinit var binding: FragmentLoginBinding
     lateinit var user: User
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +33,8 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+
         return binding.root
 
     }
@@ -38,21 +42,29 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.loginBtn.setOnClickListener {
-            binding.errorMsgEmail.isVisible = false
-            binding.errorPassword.isVisible = false
-            checkUser()
+//            binding.errorMsgEmail.isVisible = false
+//            binding.errorPassword.isVisible = false
+            if (checkUser())
+                //login
+                binding.errorMsgEmail.isVisible = false
+        }
+        binding.signupBtn.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.loginFragment_to_registerFragment)
         }
     }
-    fun checkUser(){
-        if(binding.emailTextField.toString().isEmpty() || binding.passTextField.toString().isEmpty()) {
-            if (binding.emailTextField.toString().isEmpty())
-                binding.errorMsgEmail.isVisible = true
-            if (binding.passTextField.toString().isEmpty())
-                binding.errorPassword.isVisible = true
+
+    fun checkUser() :Boolean{
+        var isValidate = true
+        if (binding.emailTextField.toString().isEmpty()) {
+            binding.errorMsgEmail.isVisible = true
+            isValidate = false
         }
-      else{
-          //Check Email, password
+        if (binding.passTextField.toString().isEmpty()) {
+            binding.errorPassword.isVisible = true
+            isValidate = false
         }
+
+        return isValidate
 
     }
 }
