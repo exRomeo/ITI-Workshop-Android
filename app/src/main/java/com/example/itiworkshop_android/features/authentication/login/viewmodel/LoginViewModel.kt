eeb309fb1930bd56ba71c5d5a475bb9da2271f21
HP <nadaelshafey99@gmail.com>
2023-05-26 17:27:34 +0200

    view model for login

--- app/src/main/java/com/example/itiworkshop_android/features/authentication/login/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/itiworkshop_android/features/authentication/login/viewmodel/LoginViewModel.kt
@@ -1,4 +1,24 @@
 package com.example.itiworkshop_android.features.authentication.login.viewmodel
 
-class LoginViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.itiworkshop_android.data.IRepository
+import com.example.itiworkshop_android.data.model.auth.LoginRequestBody
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class LoginViewModel(val repository : IRepository):ViewModel() {
+
+//
+//    private var userState: MutableStateFlow<AuthenticationResponse> = MutableStateFlow()
+
+
+    fun checkUserAuthentication(user: LoginRequestBody){
+        viewModelScope.launch {
+//           userState = RetrofitAuthenticationHelper.getRetrofitAuthenticationService().login(user)
+
+        }
+    }
 }
