19198241d1c7372f6412364ff8c0be5013562181
HP <nadaelshafey99@gmail.com>
2023-05-27 17:16:15 +0200

    implement shimmer

--- app/src/main/java/com/example/itiworkshop_android/utility/Constant.kt
+++ app/src/main/java/com/example/itiworkshop_android/utility/Constant.kt
@@ -4,5 +4,6 @@ class Constant {
 companion object{
     val emailRegex = Regex("[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}")
     val PASSWORD_PATTERN = Regex("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#\$%^&+=])(?=\\S+\$).{8,}$")
+    val ARTICLE_SAFE_ARGS_KEY = "article"
 }
 }
