c4a0d2311f285f396cddcc6323df183b8cbf44cb
karim <karim_agha5@hotmail.com>
2023-05-27 15:17:40 +0300

    m

---
+++ app/src/main/java/com/example/itiworkshop_android/features/home/details/DetailsFragment.kt
@@ -0,0 +1,32 @@
+package com.example.itiworkshop_android.features.home.details
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.itiworkshop_android.R
+import com.example.itiworkshop_android.databinding.FragmentDetailsBinding
+
+class DetailsFragment : Fragment() {
+
+    private lateinit var binding: FragmentDetailsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_details,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
